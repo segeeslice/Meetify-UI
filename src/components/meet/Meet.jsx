7c4d0519840ed25ed15ef52ff8a75d4883906b35
Dustin Seger <dustin.seger@hotmail.com>
2021-01-10 18:21:22 -0500

    Add searching transition prior to displaying meetify cards

--- src/components/meet/Meet.jsx
+++ src/components/meet/Meet.jsx
@@ -3,8 +3,16 @@
  * others with like music interests
  */
 
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import { makeStyles } from '@material-ui/core/styles'
 
+import {
+  Button,
+  CircularProgress,
+  Grid,
+  Typography,
+} from '@material-ui/core'
+import { Check } from '@material-ui/icons'
 import MeetCard from './MeetCard'
 
 const TEST_USERS = [{
@@ -21,10 +29,92 @@ const TEST_USERS = [{
   songsMatched: 5,
 }]
 
+const BUTTON_HEIGHT = 70
+const PROGRESS_HEIGHT = BUTTON_HEIGHT / 1.5
+
+const useStyles = makeStyles((theme) => ({
+  button: {
+    height: BUTTON_HEIGHT,
+  },
+  buttonProgress: {
+    color: theme.palette.primary.main,
+    position: 'absolute',
+    top: '50%',
+    left: '50%',
+    marginLeft: (-PROGRESS_HEIGHT)/2,
+    marginTop: (-PROGRESS_HEIGHT)/2,
+  },
+  initContainer: {
+    display: 'flex',
+    height: '100%',
+    width: '100%',
+  }
+}))
+
 export default function Meet () {
-  // TODO: Link operations in store
-  // TODO: Populate matches on button click?
-  const matches = TEST_USERS
+  // TODO: Move to store
+  const classes = useStyles()
+  const [matches, setMatches] = useState([])
+  const [showMatches, setShowMatches] = useState(false)
+  const [searching, setSearching] = useState(false)
+  const [buttonDisabled, setButtonDisabled] = useState(false)
+  const [success, setSuccess] = useState(false)
+
+  let timeout = null
+
+  useEffect(() => (clearTimeout(timeout)))
+
+  function findMatches () {
+    // Temporarily just swap after some delays
+    setButtonDisabled(true)
+    setSearching(true)
+    timeout = setTimeout(() => {
+      setSearching(false)
+      setMatches(TEST_USERS)
+      setSuccess(true)
+      timeout = setTimeout(() => {
+        setShowMatches(true)
+        setButtonDisabled(false)
+      }, 2000)
+    }, 2000)
+  }
+
+  // TODO: Put this logic into a re-usable component
+  // TODO: Add animation between progress and check
+  const initComp = (
+    <div className={classes.initContainer}>
+      <Grid
+        justify="center"
+        alignContent="center"
+        container
+      >
+        <Button
+          variant="contained"
+          className={classes.button}
+          size="large"
+          color="primary"
+          disabled={buttonDisabled}
+          onClick={() => findMatches()}
+        >
+          <Typography variant="h4">
+            Meetify!
+          </Typography>
+
+          {searching && !success &&
+           <CircularProgress
+             className={classes.buttonProgress}
+             size={PROGRESS_HEIGHT}
+           />}
+
+          {success &&
+           <Check
+             className={classes.buttonProgress}
+             style={{fontSize: PROGRESS_HEIGHT}}
+           />}
+        </Button>
+      </Grid>
+    </div>
+  )
 
   // TODO: Start with primary background gradient and slowly reduce opacity down list
   const meetCards = [...matches]
@@ -37,5 +127,7 @@ export default function Meet () {
       )
   })
 
-  return meetCards
+  // TODO: Add fade animation between these, possibly put into generic component first
+  if (showMatches) return meetCards
+  else return initComp
 }
